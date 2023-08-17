48601026b05d87ac6f02156c8dcaa4dbd0feead0
MaxzzzLCX <cl927@cam.ac.uk>
2023-08-17 22:41:59 +0800

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Max Lyu on 2023/8/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Max Lyu on 2023/8/17.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
