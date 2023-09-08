dcae40fddb1d55c8fcab03fd351ca68e92eee8f0
MaxzzzLCX <cl927@cam.ac.uk>
2023-09-08 14:36:46 +0800

    Lecture 4 Finished.

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -11,15 +11,15 @@ struct EmojiMemoryGameView: View {
     
     @ObservedObject var viewModel: EmojiMemoryGame = EmojiMemoryGame()//ViewModel points to View
     
-    @State var emojis: Array<String> =  []
-    let animals = ["🙈","🐥","🐶","🐱","🐻","🐧"]
-    let food = ["🍎","🍋","🥐","🍳","🍔","🌮","🍍","🥘"]
-    let vehicles = ["🚝","🚀","⛵️","🚁","🛴"]
+//    @State var emojis: Array<String> =  []
+//    let animals = ["🙈","🐥","🐶","🐱","🐻","🐧"]
+//    let food = ["🍎","🍋","🥐","🍳","🍔","🌮","🍍","🥘"]
+//    let vehicles = ["🚝","🚀","⛵️","🚁","🛴"]
     
-    @State var themeColor: Color = .green
-    
-    @State var cardCount: Int = 4
-    @State var currentTheme: String = "Animals"
+//    @State var themeColor: Color = .green
+//
+//    @State var cardCount: Int = 4
+//    @State var currentTheme: String = "Animals"
     
     
     var body: some View {
@@ -39,10 +39,10 @@ struct EmojiMemoryGameView: View {
         .padding()
     }
     
-    var title: some View{
-        Text("Memorize!")
-            .font(.largeTitle)
-    }
+//    var title: some View{
+//        Text("Memorize!")
+//            .font(.largeTitle)
+//    }
     
     
     var cards: some View{
@@ -54,79 +54,79 @@ struct EmojiMemoryGameView: View {
                     .padding(4)
             }
         }
-        .foregroundColor(themeColor)
+//        .foregroundColor(themeColor)
     }
     
     
     
-    var themeSelection: some View{
-        HStack{
-            Spacer()
-            animalTheme
-            Spacer()
-            foodTheme
-            Spacer()
-            vehicleTheme
-            Spacer()
-        }
-    }
+//    var themeSelection: some View{
+//        HStack{
+//            Spacer()
+//            animalTheme
+//            Spacer()
+//            foodTheme
+//            Spacer()
+//            vehicleTheme
+//            Spacer()
+//        }
+//    }
     
-    func themeButtonCreation(caption: String, symbol: String, themeEmojis: [String], color: Color) -> some View{
-        
-        Button(action: {
-            emojis = themeEmojis + themeEmojis
-                .shuffled()
-            themeColor = color
-        }, label: {
-            VStack{
-                Image(systemName: symbol).font(.largeTitle)
-                Text(caption)
-            }
-        })
-    }
-    
-    var animalTheme: some View{
-        themeButtonCreation(caption: "Animals", symbol: "pawprint.fill", themeEmojis: animals, color: .green)
-    }
-    var foodTheme: some View{
-        themeButtonCreation(caption: "Food", symbol: "fork.knife.circle", themeEmojis: food, color: .red)
-    }
-    var vehicleTheme: some View{
-        themeButtonCreation(caption: "Vehicles", symbol: "car.fill", themeEmojis: vehicles, color: .black)
-    }
+//    func themeButtonCreation(caption: String, symbol: String, themeEmojis: [String], color: Color) -> some View{
+//
+//        Button(action: {
+//            emojis = themeEmojis + themeEmojis
+//                .shuffled()
+//            themeColor = color
+//        }, label: {
+//            VStack{
+//                Image(systemName: symbol).font(.largeTitle)
+//                Text(caption)
+//            }
+//        })
+//    }
+//
+//    var animalTheme: some View{
+//        themeButtonCreation(caption: "Animals", symbol: "pawprint.fill", themeEmojis: animals, color: .green)
+//    }
+//    var foodTheme: some View{
+//        themeButtonCreation(caption: "Food", symbol: "fork.knife.circle", themeEmojis: food, color: .red)
+//    }
+//    var vehicleTheme: some View{
+//        themeButtonCreation(caption: "Vehicles", symbol: "car.fill", themeEmojis: vehicles, color: .black)
+//    }
     
     
     // THE FOLLOWING IS FROM LECTURE. NOT NEEDED FOR ASSIGNMENT
-    var cardCountAdjuster: some View{
-        HStack{
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
-    }
+//    var cardCountAdjuster: some View{
+//        HStack{
+//            cardRemover
+//            Spacer()
+//            cardAdder
+//        }
+//        .imageScale(.large)
+//        .font(.largeTitle)
+//    }
         
+//
+//    func cardCountAdjuster(by offset: Int, symbol: String) -> some View{
+//        Button(action: {
+//            cardCount += offset
+//        }, label: {
+//            Image(systemName: symbol)
+//        })
+//        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+//    }
+//
     
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View{
-        Button(action: {
-            cardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-        })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
-    }
     
-    
-    
-    var cardRemover: some View{
-        cardCountAdjuster(by: -1, symbol:"rectangle.stack.badge.minus.fill")
-    }
-    
-    var cardAdder: some View{
-        cardCountAdjuster(by: 1, symbol:"rectangle.stack.badge.plus.fill")
-    }
-    
+//    var cardRemover: some View{
+//        cardCountAdjuster(by: -1, symbol:"rectangle.stack.badge.minus.fill")
+//    }
+//
+//    var cardAdder: some View{
+//        cardCountAdjuster(by: 1, symbol:"rectangle.stack.badge.plus.fill")
+//    }
+//
     // THE ABOVE IS FROM LECTURE. NOT NEEDED FOR ASSIGNMENT
     
     
